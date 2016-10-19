7ef5679025a46373e69a9225108ff1f718f0e464
Kurtis <kurtis@portocloud.com>
2016-10-19 22:41:09 +0000

    Adding javascripting execrcises - not all fully completed

--- array-filtering.js
+++ array-filtering.js
@@ -1,7 +1,5 @@
-var numbers
-var numbers = [1, 2, 3, 4, 5, 6, 7, 8 ,9, 10]
-var filtered
-var filtered = numbers.filter(function evenNumbers (number) {
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8 ,9, 10];
+var filtered = numbers.filter(function evenNumbers(0)) {
   return number % 2 === 0;
 }
-console.log(1, 3, 4, 5, 6, 7, 8, 9, 10)
+console.log(numbers);
